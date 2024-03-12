6d886be2d96d880c486c562b0de10286968be7c2
Zaidfaf <155605305+Zaidfaf@users.noreply.github.com>
2024-03-12 12:17:40 -0400

    Update ailerons.cpp

--- ailerons.cpp
+++ ailerons.cpp
@@ -1,16 +1,26 @@
+//=====[Libraries]=============================================================
+
 #include "mbed.h"
 #include "arm_book_lib.h"
 #include "ailerons.h"
 #include "ignition.h"
 
+//=====[Declaration of private defines]========================================
+
 #define PERIOD_SEC              0.02
 #define DUTY_MID                0.075
+
+//=====[Declaration and initialization of public global objects]===============
 
 AnalogIn ailerons_control(A2);
 PwmOut Servo1(D14);
 PwmOut Servo2(D15);
 
+//=====[Declarations (prototypes) of private functions]========================
+
 static float aileronsPotentiometerRead();
+
+//=====[Implementations of public functions]===================================
 
 float aileronsdegrees(){
 
