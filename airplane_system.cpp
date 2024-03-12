85158bb514498f879cd2912fa0d0ba061707bb99
Zaidfaf <155605305+Zaidfaf@users.noreply.github.com>
2024-03-12 12:18:17 -0400

    Update airplane_system.cpp

--- airplane_system.cpp
+++ airplane_system.cpp
@@ -1,13 +1,18 @@
 //=====[Libraries]=============================================================
+
 #include "arm_book_lib.h"
 #include "ignition.h"
 #include "user_interface.h"
 #include "propeller.h"
 #include "sensor_monitoring.h"
 #include "ailerons.h"
+
 //=====[Declaration of private defines]========================================
+
 #define TIME_INCREMENT_MS   10
+
 //=====[Implementations of public functions]===================================
+
 void airplaneSystemInit()
 {
     ignitionInit();
