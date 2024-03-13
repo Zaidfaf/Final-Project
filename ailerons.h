e261cdb0cc61a1bed1b1fad1787792b4c1ba8187
Zaidfaf <155605305+Zaidfaf@users.noreply.github.com>
2024-03-12 21:12:11 -0400

    Update ailerons.h

--- ailerons.h
+++ ailerons.h
@@ -1,4 +1,3 @@
-
 //=====[#include guards - begin]===============================================
 
 #ifndef _AILERONS_H_
@@ -6,16 +5,22 @@
 
 //=====[Declaration of public defines]=========================================
 
+// Define the time increment in milliseconds for ailerons update
 #define TIME_INCREMENT_MS   10
 
 //=====[Declaration of public data types]======================================
 
 //=====[Declarations (prototypes) of public functions]=========================
 
+// Function to initialize aileron control
 void aileronsInit();
+
+// Function to update aileron control based on ignition status
 void aileronsUpdate();
+
+// Function to calculate and return the aileron angle in degrees
 float aileronsdegrees();
 
 //=====[#include guards - end]=================================================
 
-#endif // _AIRLERONS_H_
+#endif // _AILERONS_H_
