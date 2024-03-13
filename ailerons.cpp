a67be290b83ceef42c1d662e110f974d72fe76cc
Zaidfaf <155605305+Zaidfaf@users.noreply.github.com>
2024-03-12 21:11:06 -0400

    Update ailerons.cpp

--- ailerons.cpp
+++ ailerons.cpp
@@ -1,52 +1,49 @@
-//=====[Libraries]=============================================================
-
 #include "mbed.h"
 #include "arm_book_lib.h"
 #include "ailerons.h"
 #include "ignition.h"
 
-//=====[Declaration of private defines]========================================
-
+// Define constants for the servo control
 #define PERIOD_SEC              0.02
 #define DUTY_MID                0.075
 
-//=====[Declaration and initialization of public global objects]===============
-
+// Initialize AnalogIn and PwmOut objects for aileron control
 AnalogIn ailerons_control(A2);
 PwmOut Servo1(D14);
 PwmOut Servo2(D15);
 
-//=====[Declarations (prototypes) of private functions]========================
-
+// Private function to read the ailerons potentiometer value
 static float aileronsPotentiometerRead();
 
-//=====[Implementations of public functions]===================================
-
-float aileronsdegrees(){
-
-    return ((ailerons_control.read()-0.5)*100);
+// Public function to calculate and return aileron angle in degrees
+float aileronsdegrees() {
+    return ((ailerons_control.read() - 0.5) * 100);
 }
-static float aileronsPotentiometerRead() {//private function, .cpp file
-    float angleTarget = 0.075 + (aileronsdegrees()/1850);
+
+// Private function to convert aileron angle to servo duty cycle
+static float aileronsPotentiometerRead() {
+    float angleTarget = DUTY_MID + (aileronsdegrees() / 1850);
     return angleTarget;
 }
 
-void aileronsInit(){//public function, .h file
+// Public function to initialize aileron servo control
+void aileronsInit() {
     Servo1.period(PERIOD_SEC);
     Servo1.write(DUTY_MID);
     Servo2.period(PERIOD_SEC);
     Servo2.write(DUTY_MID);
 }
 
-void aileronsUpdate(){//public function, .h file
-    if (isIgnition()){
+// Public function to update aileron servo positions based on ignition status
+void aileronsUpdate() {
+    // Check if ignition is active
+    if (isIgnition()) {
+        // Update servo positions based on aileronsPotentiometerRead()
         Servo1.write(aileronsPotentiometerRead());
         Servo2.write(aileronsPotentiometerRead());
-    }
-    else{
+    } else {
+        // Set servo positions to mid-duty cycle if ignition is not active
         Servo1.write(DUTY_MID);
         Servo2.write(DUTY_MID);
     }
-          
 }
-
