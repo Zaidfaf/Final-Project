b2e3c05620e4ebb10fd0dfda82e81ab8d4cfdf30
PAPAN004 <155662184+PAPAN004@users.noreply.github.com>
2024-03-12 20:22:47 -0400

    Update propeller.cpp

--- propeller.cpp
+++ propeller.cpp
@@ -11,15 +11,21 @@
 #define MOTOR_UPDATE_TIME 9
 
 //=====[Declaration of private data types]=====================================
+
+typedef enum {
+    DIRECTION_1,
+    DIRECTION_2,
+    STOPPED
+} motorDirection_t;
+
 motorDirection_t motorDirection;
 motorDirection_t motorState;
+
 //=====[Declaration and initialization of public global objects]===============
 
 AnalogIn speedControl(A0);
 DigitalInOut motorM1Pin(PF_2);
 DigitalInOut motorM2Pin(PE_3);
-
-
 
 //=====[Declaration of external public global variables]=======================
 
