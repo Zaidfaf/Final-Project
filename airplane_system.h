1c0ab9426a7140d51dd7aae69e31deade4bac1c8
Zaidfaf <155605305+Zaidfaf@users.noreply.github.com>
2024-03-12 21:10:11 -0400

    Update airplane_system.h

--- airplane_system.h
+++ airplane_system.h
@@ -5,13 +5,17 @@
 
 //=====[Declaration of public defines]=========================================
 
+// Define the time increment in milliseconds for the system update
 #define TIME_INCREMENT_MS   10
 
 //=====[Declaration of public data types]======================================
 
 //=====[Declarations (prototypes) of public functions]=========================
 
+// Function to initialize the airplane system components
 void airplaneSystemInit();
+
+// Function to update the airplane system components periodically
 void airplaneSystemUpdate();
 
 //=====[#include guards - end]=================================================
